fe2bf473edf61744bd947ccb5ac9b3000495af14
waiphyonaing002 <waip47809@gmail.com>
2025-05-19 16:12:21 +0630

    Database connection.

--- src/main/java/com/db_test/Main.java
+++ src/main/java/com/db_test/Main.java
@@ -1,9 +1,41 @@
 package com.db_test;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+    private String ip = "localhost";
+    private String port = "3306";
+    private String dbname = "test_db";
+    private String username = "root";
+    private String password = "";
+
+    protected Connection get_Db_Connection(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + dbname, username, password);
+            System.out.println("Successful Connection.");
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return con;
+    }
     public static void main(String[] args) {
-    System.out.println("Hello world");
+        Main m = new Main();
+        Connection con = m.get_Db_Connection();
+
+        try{
+            if(con !=null) {
+                con.close();
+                System.out.println("Connection is closed.");
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+
+        }
+
         }
     }
